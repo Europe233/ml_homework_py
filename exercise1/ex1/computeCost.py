4486eeb7c8f6b60dedaeccb4cdcaebe1c4608092
Europe233 <38278391+Europe233@users.noreply.github.com>
2019-04-23 17:29:33 +0800

    Add files via upload

--- exercise1/ex1/computeCost.py
+++ exercise1/ex1/computeCost.py
@@ -3,11 +3,10 @@ import numpy as np
 def  cost(theta,x,y):
     """计算对应参数的Cost"""
     #数据集样本数
-    m = x.size
+    m = y.size
 
     #计算cost
-    cost = 0
-    for i in range(m):
-        err = theta[0] + theta[1]*x[i] - y[i]
-        cost += (err * err)/ (2 * m)
+    err_vec = np.dot(x,theta)-y
+    cost = np.dot(err_vec,err_vec)/(2*m)
+
     return cost
--- exercise1/ex1/gradientDescent.py
+++ exercise1/ex1/gradientDescent.py
@@ -1,18 +1,22 @@
 import numpy as np
 
-from computeCost import *
+def gradient_theta(theta,x,y):
+    """返回参数的梯度（其反方向即为下一步迭代的搜索方向）"""
+    m=y.size
+
+    gra_theta = np.dot(np.dot(theta.T,x.T),x) - np.dot(y.T,x)
+    gra_theta = gra_theta/m
+
+    return gra_theta
 
 def next_parameter(alpha,theta,x,y):
     """返回下一步的参数theta"""
     #样本数
-    m = x.size
-    new_theta = theta.copy()
+    m = y.size
 
     #参数迭代
-    for i in range(m):
-        new_theta[0] = new_theta[0]-alpha*(theta[0]+theta[1]*x[i]-y[i])/m
-        new_theta[1] = new_theta[1]-alpha*(theta[0]+theta[1]*x[i]-y[i])*x[i]/m
-
+    new_theta=theta-alpha*gradient_theta(theta,x,y)
+    
     return new_theta
 
 def gradient_descent(alpha,ini_theta,maxIteration,acceptable_err,x,y):
@@ -27,7 +31,7 @@ def gradient_descent(alpha,ini_theta,maxIteration,acceptable_err,x,y):
         theta_list.append(next_theta)
 
         #结束条件：超过最大迭代次数 or 误差可接受
-        if abs(cost(cnt_theta,x,y)-cost(next_theta,x,y)) < acceptable_err:
+        if np.linalg.norm(gradient_theta(next_theta,x,y))<acceptable_err:
             print("迭代次数：",len(theta_list))
             break
         if len(theta_list) > maxIteration:
